aeae6781cc7d1488886f83e0dba6971d1704e09c
noxteryn <noxteryn@gmail.com>
2023-04-13 13:17:26 +0300

    First commit.

---
+++ src/main/java/com/github/noxteryn/expensetracker/ExpenseTrackerApplication.java
@@ -0,0 +1,26 @@
+package com.github.noxteryn.expensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Create a RESTful API for managing expenses.
+// Users can create, retrieve, update, and delete expense records through REST endpoints.
+// The expense data can be stored in a database such as MySQL or Postgresql.
+// You can implement features such as authentication and authorization to secure the API endpoints.
+// After you build your application, incorporate your jar file in a docker container.
+// Then invoke the exposed APIs of the container.
+// You can invoke the APIs using Postman.
+
+// Expense tracking requires:
+// 1. Amount of expenditure. Type: Currency
+// 2. Date of expenditure. Type: Date
+// 3. Purpose of expenditure. Type: String
+
+@SpringBootApplication
+public class ExpenseTrackerApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ExpenseTrackerApplication.class, args);
+	}
+}
